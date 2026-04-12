178565e8370503fdd00c9c9bdd12f206815a0c5f
Ricardo Grana <rickgrana@gmail.com>
2026-04-12 17:06:52 -0400

    Adição dos enums das regiões do corpo

---
+++ src/app/const/regioes-corpo-frente.ts
@@ -0,0 +1,131 @@
+export const REGIOES_CORPO_FRENTE = new Map<string, string>([
+  ["1", "Região Frontal"],
+
+  ["2D", "Região Orbitária Direita"],
+  ["2E", "Região Orbitária Esquerda"],
+
+  ["3", "Região Nasal"],
+
+  ["4D", "Região Malar Direita"],
+  ["4E", "Região Malar Esquerda"],
+
+  ["5D", "Região Masseterina Direita"],
+  ["5E", "Região Masseterina Esquerda"],
+
+  ["6D", "Região Bucinadora Direita"],
+  ["6E", "Região Bucinadora Esquerda"],
+
+  ["7", "Região Labial"],
+  ["8", "Região Mentoniana"],
+
+  ["9", "Região Supra-hióidea"],
+  ["10", "Região Infra-hióidea"],
+
+  ["11D", "Região Carotidiana Direita"],
+  ["11E", "Região Carotidiana Esquerda"],
+
+  ["12D", "Região Supra-clavicular Direita"],
+  ["12E", "Região Supra-clavicular Esquerda"],
+
+  ["13D", "Região Clavicular Direita"],
+  ["13E", "Região Clavicular Esquerda"],
+
+  ["14D", "Região Infraclavicular Direita"],
+  ["14E", "Região Infraclavicular Esquerda"],
+
+  ["15", "Região Esternal"],
+
+  ["16D", "Região Torácica Direita"],
+  ["16E", "Região Torácica Esquerda"],
+
+  ["17D", "Região Mamária Direita"],
+  ["17E", "Região Mamária Esquerda"],
+
+  ["18", "Região Epigástrica"],
+
+  ["19D", "Região Hipocôndrica Direita"],
+  ["19E", "Região Hipocôndrica Esquerda"],
+
+  ["20", "Região Mesogástrica"],
+
+  ["21", "Região Umbilical"],
+
+  ["22D", "Região Flanco Direito"],
+  ["22E", "Região Flanco Esquerdo"],
+
+  ["23", "Região Hipogástrica"],
+
+  ["24D", "Região Fossa Ilíaca Direita"],
+  ["24E", "Região Fossa Ilíaca Esquerda"],
+
+  ["25", "Região Pubiana"],
+
+  ["26D", "Região Inguinal Direita"],
+  ["26E", "Região Inguinal Esquerda"],
+
+  ["27D", "Região Crural Direita"],
+  ["27E", "Região Crural Esquerda"],
+
+  ["28", "Região Peniana"],
+  ["29", "Região Escrotal"],
+
+  ["30D", "Terço Superior do Braço Direito"],
+  ["30E", "Terço Superior do Braço Esquerdo"],
+
+  ["31D", "Terço Médio do Braço Direito"],
+  ["31E", "Terço Médio do Braço Esquerdo"],
+
+  ["32D", "Terço Inferior do Braço Direito"],
+  ["32E", "Terço Inferior do Braço Esquerdo"],
+
+  ["33D", "Região do Cotovelo Direito"],
+  ["33E", "Região do Cotovelo Esquerdo"],
+
+  ["34D", "Terço Superior do Antebraço Direito"],
+  ["34E", "Terço Superior do Antebraço Esquerdo"],
+
+  ["35D", "Terço Médio do Antebraço Direito"],
+  ["35E", "Terço Médio do Antebraço Esquerdo"],
+
+  ["36D", "Terço Inferior do Antebraço Direito"],
+  ["36E", "Terço Inferior do Antebraço Esquerdo"],
+
+  ["37D", "Punho Direito"],
+  ["37E", "Punho Esquerdo"],
+
+  ["38D", "Dorso da Mão Direita"],
+  ["38E", "Dorso da Mão Esquerda"],
+
+  ["39D", "Terço Superior da Coxa Direita"],
+  ["39E", "Terço Superior da Coxa Esquerda"],
+
+  ["40D", "Terço Médio da Coxa Direita"],
+  ["40E", "Terço Médio da Coxa Esquerda"],
+
+  ["41D", "Terço Inferior da Coxa Direita"],
+  ["41E", "Terço Inferior da Coxa Esquerda"],
+
+  ["42D", "Região Rotuliana Direita"],
+  ["42E", "Região Rotuliana Esquerda"],
+
+  ["43D", "Região Anterior do Joelho Direito"],
+  ["43E", "Região Anterior do Joelho Esquerdo"],
+
+  ["44D", "Terço Superior da Perna Direita"],
+  ["44E", "Terço Superior da Perna Esquerda"],
+
+  ["45D", "Terço Médio da Perna Direita"],
+  ["45E", "Terço Médio da Perna Esquerda"],
+
+  ["46D", "Terço Inferior da Perna Direita"],
+  ["46E", "Terço Inferior da Perna Esquerda"],
+
+  ["47D", "Lateral Externa da Perna Direita"],
+  ["47E", "Lateral Externa da Perna Esquerda"],
+
+  ["48D", "Lateral Interna da Perna Direita"],
+  ["48E", "Lateral Interna da Perna Esquerda"],
+
+  ["49D", "Dorso do Pé Direito"],
+  ["49E", "Dorso do Pé Esquerdo"]
+]);
---
+++ src/app/const/regioes-corpo-verso.ts
@@ -0,0 +1,83 @@
+export const REGIOES_CORPO_VERSO = new Map<string, string>([
+  ["1D", "Região Parietal Direita"],
+  ["1E", "Região Parietal Esquerda"],
+
+  ["2", "Região Occipital"],
+
+  ["3D", "Região Temporal Direita"],
+  ["3E", "Região Temporal Esquerda"],
+
+  ["4", "Região Posterior do Pescoço (Nuca)"],
+  ["5", "Região Vertebral"],
+
+  ["6D", "Região Escapular Direita"],
+  ["6E", "Região Escapular Esquerda"],
+
+  ["7D", "Região Infra-escapular Direita"],
+  ["7E", "Região Infra-escapular Esquerda"],
+
+  ["8D", "Região Lombar Direita"],
+  ["8E", "Região Lombar Esquerda"],
+
+  ["9D", "Região Lateral do Abdome Direita"],
+  ["9E", "Região Lateral do Abdome Esquerda"],
+
+  ["10", "Região Sacral"],
+
+  ["11D", "Região Glútea Direita"],
+  ["11E", "Região Glútea Esquerda"],
+
+  ["12D", "Terço Proximal da Coxa Direita"],
+  ["12E", "Terço Proximal da Coxa Esquerda"],
+
+  ["13D", "Terço Médio da Coxa Direita"],
+  ["13E", "Terço Médio da Coxa Esquerda"],
+
+  ["14D", "Terço Distal da Coxa Direita"],
+  ["14E", "Terço Distal da Coxa Esquerda"],
+
+  ["15D", "Região Posterior do Joelho Direito"],
+  ["15E", "Região Posterior do Joelho Esquerdo"],
+
+  ["16D", "Terço Proximal da Perna Direita"],
+  ["16E", "Terço Proximal da Perna Esquerda"],
+
+  ["17D", "Terço Médio da Perna Direita"],
+  ["17E", "Terço Médio da Perna Esquerda"],
+
+  ["18D", "Terço Distal da Perna Direita"],
+  ["18E", "Terço Distal da Perna Esquerda"],
+
+  ["19D", "Região Calcânea Direita"],
+  ["19E", "Região Calcânea Esquerda"],
+
+  ["20D", "Dorso do Pé Direito"],
+  ["20E", "Dorso do Pé Esquerdo"],
+
+  ["21D", "Região Deltoidea Direita"],
+  ["21E", "Região Deltoidea Esquerda"],
+
+  ["22D", "Terço Proximal do Braço Direito"],
+  ["22E", "Terço Proximal do Braço Esquerdo"],
+
+  ["23D", "Terço Médio do Braço Direito"],
+  ["23E", "Terço Médio do Braço Esquerdo"],
+
+  ["24D", "Terço Distal do Braço Direito"],
+  ["24E", "Terço Distal do Braço Esquerdo"],
+
+  ["25D", "Região Posterior do Cotovelo Direito"],
+  ["25E", "Região Posterior do Cotovelo Esquerdo"],
+
+  ["26D", "Terço Proximal do Antebraço Direito"],
+  ["26E", "Terço Proximal do Antebraço Esquerdo"],
+
+  ["27D", "Terço Médio do Antebraço Direito"],
+  ["27E", "Terço Médio do Antebraço Esquerdo"],
+
+  ["28D", "Terço Distal do Antebraço Direito"],
+  ["28E", "Terço Distal do Antebraço Esquerdo"],
+
+  ["29D", "Dorso da Mão Direita"],
+  ["29E", "Dorso da Mão Esquerda"]
+]);
